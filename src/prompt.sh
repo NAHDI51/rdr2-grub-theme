0ff6198946ab58ff2c6c8597602f8204a653402b
NAHDI51 <hossainnahdi8051@gmail.com>
2022-12-13 19:17:21 +0600

    colorize_output: added dependency and will start working on the function

---
+++ src/prompt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This file handles output formatting and colorization of the console 
+# output. This will take standard color codes of @background and
+# @foreground, and output accordingly. Prompt 
+# 
+# 
+# 
+
+# colorize_output 
+#
+# Colorizes the output according to the flags. Flags are:
+#
+
+colorize_output() {
+    local helpmsg=("
+        usage: colorize_output [-options ...] [texts]
+        
+    ")
+}
