4fbfc2886c89d63d1131692c9de426e58d0b0a46
NAHDI51 <hossainnahdi8051@gmail.com>
2022-12-14 23:46:08 +0600

    implementation changes: color option now works like modes, with modes being foreground and background

--- src/colors.sh
+++ src/colors.sh
@@ -6,7 +6,7 @@
 # Check if this file has previously been defined
 # If not, define it, so that other files can include it.
 
-if [${echo -e $__COLOR_SH} -eq ""]; then
+if [$(echo -e $__COLOR_SH) -eq ""]; then
     __COLOR_SH="1"
 fi
 
@@ -14,57 +14,40 @@ fi
 BEG="\e["
 END="\e[0m"
 
-# D = DARK
-# L = LIGHT
+# D=DARK
+# L=LIGHT
 
-# Background codes
-B_BLACK="40"
-B_RED="41"
-B_GREEN="42"
-B_BROWN="43"
-B_BLUE="44"
-B_PURPLE="45"
-B_CYAN="46"
-B_LGRAY"47"
+BG_MODE=0
+FG_MODE=1
 
-# Foreground codes
-F_BLACK="30"
-F_RED="31"
-F_GREEN="32"
-F_BROWN="33"
-F_BLUE="34"
-F_PURPLE="35"
-F_CYAN="36"
-F_LGRAY="37"
+# (Background codes, Foreground codes)
+BLACK=("40" "30")
+RED=("41" "31")
+GREEN=("42" "32")
+BROWN=("43" "43")
+BLUE=("44" "34")
+PURPLE=("45" "35")
+CYAN=("46" "36")
+LGRAY=("47" "37")
 
 # formats
+
 NORMAL="0"
 UNDERLINED="4"
 BLINKING="5"
+#Bold isn't present here due to having different color contexts.
 
 # There, however, are some differences of codes, when we use
 # bold fonts. Thus, we need to define them distinctly.
 
-# Bold background codes
+# Bold (background codes, foreground codes)
 
-BB_DGRAY="1;40"
-BB_LRED="1;41"
-BB_LGREEN="1;42"
-BB_YELLOW="1;43"
-BB_LBLUE="1;44"
-BB_LPURPLE="1;45"
-BB_LCYAN="1;46"
-BB_WHITE="1;47"
-
-# Bold foreground codes
-
-BF_DGRAY="1;30"
-BF_LRED="1;31"
-BF_LGREEN="1;32"
-BF_YELLOW="1;33"
-BF_LBLUE="1;34"
-BF_LPURPLE="1;35"
-BF_LCYAN="1;36"
-BF_WHITE="1;37"
-
+B_DGRAY=("1;40" "1;30")
+B_LRED=("1;41" "1;31")
+B_LGREEN=("1;42" "1;32")
+B_YELLOW=("1;43" "1;33")
+B_LBLUE=("1;44" "1;34")
+B_LPURPLE=("1;45" "1;35")
+B_LCYAN=("1;46" "1;36")
+B_WHITE=("1;47" "1;37")
 
