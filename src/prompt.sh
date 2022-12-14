d545998d1f254d5bed7e58c98039c3f36a1b6636
NAHDI51 <hossainnahdi8051@gmail.com>
2022-12-14 23:47:59 +0600

    Added: print help option

--- src/prompt.sh
+++ src/prompt.sh
@@ -22,11 +22,52 @@ fi
 
 colorize_output() {
     local helpmsg=("
-        Usage: colorize_output [-options ...] [texts] \n
+        Usage: colorize_output [-options] ... [text] \n
         \n
-        Colorize the output using various options. Call this function with the \n
-        background and foreground color specified.
+        Colorize the output using various options. Call this function with the  \n
+        background and foreground color operations specified. \n
+        \n
+        OPERATIONS \n
+        -B | --background [FORMAT] [COLOR] \t Format the background text \n
+        -F | --foreground [FORMAT] [COLOR] \t Format the foreground text \n
+        -h | --help \t \t \t \t               Print this text
+        \n
+        FORMATS\n
+        -r | --regular \n 
+        -b | --bold \n
+        -u | --underlined \t  Underlines the text \n
+        -l | --blinking \t    Text blinks \n
+        \n
+        \n
+        COLORS\n
+        There are some different options available for bold formatting. Thus, \n 
+        we cannot specify different exclusive colors to the vice versa. \n
+        \n
+        COLOR OPTION FLAGS FOR REGULAR/UNDERLINE/BLINK \n
+        --black \n
+        --red \n
+        --green \n 
+        --brown \n 
+        --blue \n 
+        --purple \n 
+        --cyan \n 
+        --light-gray \n
+        \n
+        COLOR OPTION FLAGS FOR BOLD \n
+        --dark-gray \n
+        --light-red \n
+        --light-green \n 
+        --yellow \n 
+        --light-blue \n 
+        --light-purple \n 
+        --light-cyan \n
+        --white \n
+        \n
+        \n
+        DEFAULTS
+        FORMAT     = Regular \n
+        COLOR      = White \n
+        Foreground = White regular text \n
+        Background = No background \n
     ")
-
-    echo -e $helpmsg
 }
