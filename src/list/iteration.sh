cb66ede65f1e5c6fc969002242757f78bd1a710b
NAHDI51 <hossainnahdi8051@gmail.com>
2023-05-27 22:00:41 +0600

    Finished writing and debugging: the code works as expected so far

--- src/list/iteration.sh
+++ src/list/iteration.sh
@@ -18,9 +18,9 @@
 # <li> type, like 1,a, or A. This file is created with the intention
 # to use it in a choice-based system. 
 
-# Define source code variable: __LIST_SH
-if [[ "$__LIST_SH" -ne "1" ]]; then
-__LIST_SH=1
+# Define source code variable: __ITERATION_SH
+if [[ "$__ITERATION_SH" -ne "1" ]]; then
+__ITERATION_SH=1
 fi
 
 # Include include
@@ -40,7 +40,7 @@ fi
 # Check helpmsg for all details. 
 
 iterate() {
-    helpmsg = ('
+    local helpmsg=('
 iterate TYPE ARGUMENT(S)
 
 iterate function creates an ordered list of the Arguments provided, 
@@ -59,6 +59,7 @@ please do so from the source code itself.
 
 Run iterate --help to view this message. 
 ')
+    local LOG_HEADER="src/list/iteration.sh"
 
     # help message display
     case "$1" in 
@@ -68,4 +69,75 @@ Run iterate --help to view this message.
             ;;
     esac
 
+    # The function will have at least 1 mandatory argument (provided
+    # that there can be 0 or more optional list arguments)
+
+    if [[ $# < 1 ]]; then 
+        colorize_output -F --yellow -B --red "$LOG_HEADER: Warning: insufficient arguments to call the function. Exitting the function with ERROR CODE: 127\n"
+        return 127
+    fi 
+
+    local LIST_TYPE=$1
+    shift 1
+    local ARGS=("$@")  # Store arguments in an array
+
+    if [[ $LIST_TYPE == 'a' || $LIST_TYPE == 'A' ]] && (( ${#ARGS[@]} > 26 )); then
+        colorize_output -F --yellow -B --red "$LOG_HEADER: Warning: too many arguments for the type: $LIST_TYPE. Using TYPE 1 instead."
+        LIST_TYPE='1'
+    fi
+    
+    local n=${#ARGS[@]}
+    local i=0
+
+    # First and last bound of the list
+    local char=''
+    local first_char=''
+
+    for ((i = 0; i < $n; i++)); do
+        # POTENTIAL CONFLICT BETWEEN GLOBAL VARIABLES
+        # colorize_output function's ITERATIVE variable $i is global, thus
+        # it can meddle with this code's i iteration. This can potentially
+        # cause an infinite loop.
+        # 
+        # The workaround is to preserve the value of $i in this function, in
+        # a temporary variable called idx, and use it whenwever needed. This,
+        # however, is just a temporary solution, and some fixes need to be done
+        # with this control flow exception.
+
+        local idx=$i
+
+        case "$LIST_TYPE" in
+            'a')
+                # ASCII of 'a'+$i
+                char=$(printf "\\$(printf '%03o' $((i+97)))")
+                ;;
+            'A')
+                # ASCII of 'A'+$i
+                char=$(printf "\\$(printf '%03o' $((i+65)))")
+                ;;
+            '1')
+                # No need to do ASCII. Use decimal instead
+                char=$((i+1))
+                ;;
+            *)
+                colorize_output -F --red -B --black "$LOG_HEADER: FATAL ERROR: $LIST_TYPE is not a valid LIST TYPE. Exiting the function with code 127.\n" 
+                i=$idx
+                exit 127
+                ;;
+        esac
+
+        if (( i == 0 )); then 
+            first_char=$char
+        fi
+
+        # Why did I ever choose to torture myself with bash script T_T
+        colorize_output -F --red -B --black "$char." && i=$idx
+        colorize_output -F --blue "${ARGS[i]}" && i=$idx
+        echo -en "\n"
+    done
+
+    unset i
+
+    local RETURN_VALUE=("$first_char" "$char")
+    echo "${RETURN_VALUE[@]}"  # Return the array elements
 }
