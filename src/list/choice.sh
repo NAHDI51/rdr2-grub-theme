97b789a4aa65518a3ab71623075d8a85bffe6b32
NAHDI51 <hossainnahdi8051@gmail.com>
2023-05-30 01:03:15 +0600

    askChoices implemented, but uncommented and soft-tested. Need to comment it, and test it more.

--- src/list/choice.sh
+++ src/list/choice.sh
@@ -42,12 +42,105 @@ if [[ "$__ALIASES_SH" -eq "" ]]; then
     include 'src/aliases.sh'
 fi 
 
-# Choice from a range: [A-Z], [a-z], or [1-N]...
-# Two arguments, specifying the first and the last.
+# Asks for the choice
+askChoices() { 
 
-ask() {
+    local MAIN_PROMPT="Please enter your choice"
+    local HELP_PROMPT="(Press ~ to view the list of choices)"
+    local ERR_PROMPT="[Invalid Choice]"
+
+    local LAST_ERROR=0         # If the last prompt is ERROR or not
+    local CHOICE_NOT_CHOSEN=1    # Turns false if the choice has been chosen
+
+    local FINAL_CHOICE=""
+    local CHOICE=''
+
+    while (( $CHOICE_NOT_CHOSEN == 1 )); do 
+        echo "$CHOICE_NOT_CHOSEN $LAST_ERROR"
+        if (( $LAST_ERROR == 1)); then 
+            colorize_output -F --red -B --black "$ERR_PROMPT "
+        fi 
+
+        read -p "$MAIN_PROMPT $HELP_PROMPT: " CHOICE
+
+        if [[ "$CHOICE" == '~' ]]; then 
+            colorize_output -F --green -B --black "Choices:"
+            echo -en "\n"
+
+            # Print all the keys
+            for KEY in "${!CHOICES[@]}"; do 
+                echo -en "$KEY\t"
+            done 
+            echo -en "\n"
+
+            LAST_ERROR=0
+            CHOICE_NOT_CHOSEN=1
+
+        elif [[ -v CHOICES["$CHOICE"] ]]; then 
+            FINAL_CHOICE="$CHOICE"
+
+            LAST_ERROR=0
+            CHOICE_NOT_CHOSEN=0
+
+        else 
+            LAST_ERROR=1
+            CHOICE_NOT_CHOSEN=1
+        fi
+        
+    done 
+    
+    echo "$FINAL_CHOICE"
 }
 
+choiceCustom() {
+    local helpmsg=('
+SYNTAX: choiceCustom [CHOICES...]
+
+The choiceCustom function takes an array of choices, and thinks
+of them as custom choices. 
+
+Example:
+
+choiceCustom a b c d e
+This call consideres 5 individual choices: a, b, c, d, and e.
+
+choiceCustom 1 a c 2 b f 6
+This call consideres 7 individual choices: a, b, c, f, 1, 2, and 6.
+
+@return value: the choice chosen
+')
+    eval $PRINT_HELPMSG
+    
+    local LOG_HEADER="src/list/choice.sh/choiceCustom()"
+    local ARGS=("$@")
+    local n=$#
+    
+    # Store the choices in the associative array, and call ask 
+    declare -A CHOICES 
+    for (( i = 0; i < $n; i++ )); do 
+        local idx=$i 
+
+        # If the element already exists 
+        if [[ -v CHOICES["${ARGS[i]}"] ]]; then 
+            colorize_output -F --red -B --black "$LOG_HEADER: Warning: '${ARGS[i]}' has multiple instances.\n"
+            i=$idx
+            continue 
+        fi 
+
+        CHOICES["${ARGS[i]}"]=true 
+    done 
+
+    # unset global variables that could cause potential conflict
+    unset i
+
+    #Invoke the choice based system
+    askChoices CHOICES
+}
+
+# Wrappers of choiceCustom choiceRange and choiceYN
+
+# Choice from a range: [A-Z], [a-z], or [1-N]...
+# Two arguments, specifying the first and the last.
 choiceRange() {
     local helpmsg=('
 SYNTAX: choiceRange [START] [END]
@@ -77,6 +170,7 @@ This will query between the choices: [1-29].
 
 }
 
+# SElf explanatory
 choiceYN() {
     local helpmsg=('
 SYNTAX: choiceYN 
@@ -85,11 +179,8 @@ The choiceYN function creates a choice based system dependant
 on Y or N. Thus, no user argument is to be provided. 
 
 @return value: the choice chosen
-    ')
+')
 
     eval "$PRINT_HELPMSG"
-}
-
-choiceCustom() {
-    echo "Hello world"
+    choiceCustom y n
 }
