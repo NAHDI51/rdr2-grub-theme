c26fe2efcd1bb6e1fd86ab8ad1e5bbbbc7db7379
NAHDI51 <hossainnahdi8051@gmail.com>
2023-06-02 00:01:50 +0600

    Added a global helper:  (details commented)

--- src/aliases.sh
+++ src/aliases.sh
@@ -7,6 +7,7 @@ if [[ "$__ALIASES_SH" -ne "1" ]]; then
     __ALIASES_SH=1
 fi
 
+# Prints helpmsg with the according arguments, if asked. 
 PRINT_HELPMSG=('
     case "$1" in 
         "-h" | "--help" | "--Help" )
@@ -16,3 +17,13 @@ PRINT_HELPMSG=('
     esac
 ')
 
+# A global helper variable that stores the last returned value of
+# a non-void variable. this variable is introducted due to the fact
+# that bash functions cannot return non-integer values: they are
+# designed for diplsaying program status.
+
+# Also note that, for non-interactive functions, you can simply 
+# do echo, an store the variable using substitution. This is a solid
+# alternative, as it creates an almost identical return-value facade.
+
+RETURN_VALUE=''
