3a625aebf50abde309548aa0f1ab8db63b9d0efe
NAHDI51 <hossainnahdi8051@gmail.com>
2023-06-12 02:57:51 +0600

    Removed themes, and replaced with build

--- add_entry.sh
+++ add_entry.sh
@@ -2,7 +2,10 @@
 
 for ENTRY in wallpapers/*; do 
     NAME=$(echo $ENTRY | awk -F'/' '{ print $NF }')
-    mkdir create/$NAME 
-    cp -r ../arthur-morgan/* create/$NAME
-    cp -a $ENTRY/background.png create/$NAME/background.png
+    # mkdir -p build
+    tar -czvf build/$NAME.tar.gz create/$NAME
+
+    # mkdir create/$NAME 
+    # cp -r ../arthur-morgan/* create/$NAME
+    # cp -a $ENTRY/background.png create/$NAME/background.png
 done 
